ae272176e6442b850cb8ba17ef6f0f7ce26f79c2
Matt Despain <mattdespainphotovideo@gmail.com>
2023-10-21 02:42:17 -0600

    view

--- ShareNotes/notes/views.py
+++ ShareNotes/notes/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from notes.functions import handle_uploaded_file  
 from notes.models import StudentForm  
+from django.contrib.auth.decorators import permission_required
+from notes.models import Classroom
+from notes.models import UploadedFile
+
 
 # Create your views here.
 def index(request, first_name): 
@@ -17,4 +21,10 @@ def index(request, first_name):
     else:  
         student = StudentForm()  
         return render(request,"index.html",{'form':form})  
+
+@permission_required('your_app.view_classroom_files', (Classroom, 'id', 'classroom_id'))
+def view_classroom_files(request, classroom_id):
+    classroom = Classroom.objects.get(id=classroom_id)
+    files = UploadedFile.objects.filter(classroom=classroom)
+    return render(request, 'classroom_files.html', {'files': files})
 
