78da17443c12b118ce4d655bd4609e2a14b4722e
Jared Smith <smithvjared@gmail.com>
2023-10-21 17:14:49 -0600

    standardized upload and splash pages

--- ShareNotes/ShareNotes/settings.py
+++ ShareNotes/ShareNotes/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-tkx!l12t(dp$eui62zjen7i&3g^f+t=j0vn*y_uu%aujjsvb87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app",
+        '127.0.0.1', 'localhost'
+]
 
 
 # Application definition
--- ShareNotes/registration/urls.py
+++ ShareNotes/registration/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # put your paths here
     path("registration/", views.registration, name="registration"),
     path("login/", views.log_in, name="login"),
+    path("log_out/", views.log_out, name="logout")
 ]
