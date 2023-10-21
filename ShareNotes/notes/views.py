875f2a7c939853186882757ddef8789ef24f39f8
Matt Despain <mattdespainphotovideo@gmail.com>
2023-10-20 23:28:35 -0600

    user model and form

--- ShareNotes/notes/views.py
+++ ShareNotes/notes/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from notes.functions import handle_uploaded_file  
+from notes.models import StudentForm  
 
 # Create your views here.
-
-def index(request):
-    return HttpResponse("<h1>Hello, world<h1>")
+def index(request):  
+    if request.method == 'POST':  
+        student = StudentForm(request.POST, request.FILES)  
+        if student.is_valid():  
+            handle_uploaded_file(request.FILES['file'])  
+            return HttpResponse("File uploaded successfuly")  
+    else:  
+        student = StudentForm()  
+        return render(request,"index.html",{'form':student})  
 
